215a8261f95a8d498726ab8b19a4382894d5cfe5
Nilo Siqueira <nilosiqueiratj@gmail.com>
2022-05-24 21:48:35 -0300

    add database

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("Olá Mundo!")
-})
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá Nlw método Post")
-})
 
 app.listen(3000, () => console.log("Server is running"));
